ef0bae360d83fed5eee3841674c20731e802e8fb
Karan Thukral <karanthukral@gmail.com>
2022-02-22 13:03:05 -0500

    add dig command

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"net"
+	"os/exec"
 )
 
 func main() {
 	domain := "dc.services.visualstudio.com"
+
+	cmd := exec.Command("dig", domain)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("dig ERROR: %s\n", err.Error())
+	} else {
+		fmt.Printf("dig out: %s\n", string(out))
+	}
+
 	ips, err := net.LookupIP(domain)
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err.Error())
